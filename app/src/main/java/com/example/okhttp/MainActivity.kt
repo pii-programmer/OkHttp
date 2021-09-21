d7500724cc0e757dafbd169937c0b1148df6559c
pii.programmer <pii.programmer@gmail.com>
2021-09-21 19:01:15 +0900

    onResponse内をmainスレッドに渡す

--- app/src/main/java/com/example/okhttp/MainActivity.kt
+++ app/src/main/java/com/example/okhttp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.okhttp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.example.okhttp.databinding.ActivityMainBinding
 import okhttp3.*
 import java.io.IOException
@@ -11,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     lateinit var client: OkHttpClient
     lateinit var request: Request
+    lateinit var handler: Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,24 +21,29 @@ class MainActivity : AppCompatActivity() {
 
         // bindingの初期化
         binding = ActivityMainBinding.inflate(layoutInflater)
+        // handlerの初期　メインスレッドを指定
+        handler = Handler(Looper.getMainLooper())
         // clientの初期化
         client = OkHttpClient()
         // requestの初期化
-        request = Request.Builder().url("https://weather.tsukumijima.net/api/forecast/city/130010").build()
+        request = Request.Builder().url("https://weather.tsukumijima.net/api/forecast/city/130010").get().build()
 
-        // バックグラウンドでCallbackメソッド
-        client.newCall(request).enqueue(object: Callback{
-            // 通信失敗時
-            override fun onFailure(call: Call, e: IOException) {
-                println("Error")
-            }
-            // レスポンス処理
-            override fun onResponse(call: Call, response: Response) {
-                println("It's OK")
-            }
-        })
         binding.tomorrow.setOnClickListener {
-            binding.result.text = "Hello World"
+            // バックグラウンドでCallbackメソッド
+            client.newCall(request).enqueue(object: Callback{
+                // 通信失敗時
+                override fun onFailure(call: Call, e: IOException) {
+//                    handler.post{
+//                        binding.result.text = "Error"
+//                    }
+                }
+                // レスポンス処理
+                override fun onResponse(call: Call, response: Response) {
+                    handler.post{
+                        binding.result.text = "It's OK"
+                    }
+                }
+            })
         }
     }
 }

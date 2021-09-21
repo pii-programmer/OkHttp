4d8e5c3994873d2f7b3620f5b90992a1202897b2
pii.programmer <pii.programmer@gmail.com>
2021-09-21 17:31:37 +0900

    viewBindingとクリックリスナー

--- app/src/main/java/com/example/okhttp/MainActivity.kt
+++ app/src/main/java/com/example/okhttp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.okhttp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.okhttp.databinding.ActivityMainBinding
 import okhttp3.*
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     lateinit var client: OkHttpClient
     lateinit var request: Request
 
@@ -13,15 +16,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // bindingの初期化
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        // clientの初期化
         client = OkHttpClient()
+        // requestの初期化
         request = Request.Builder().url("https://weather.tsukumijima.net/api/forecast/city/130010").build()
+
+        // バックグラウンドでCallbackメソッド
         client.newCall(request).enqueue(object: Callback{
+            // 通信失敗時
             override fun onFailure(call: Call, e: IOException) {
                 println("Error")
             }
+            // レスポンス処理
             override fun onResponse(call: Call, response: Response) {
                 println("It's OK")
             }
         })
+        binding.tomorrow.setOnClickListener {
+            binding.result.text = "Hello World"
+        }
     }
 }
